9781fddcbe54ad0277b635576a6a9675dda3a66a
zuvy <zuvanich@gmail.com>
2022-04-22 22:32:11 -0500

    Edit and Update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,27 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #render plain: @article.inspect
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    byebug
+  end
 end
